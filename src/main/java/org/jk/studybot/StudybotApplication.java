da159711d73ea3a438b36432871d30c8687a3351
Jung-Kwon Lee <73648662+leejk0924@users.noreply.github.com>
2025-09-15 03:56:29 +0900

    init: set Project

---
+++ src/main/java/org/jk/studybot/StudybotApplication.java
@@ -0,0 +1,13 @@
+package org.jk.studybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudybotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudybotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jk/studybot/StudybotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jk.studybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudybotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
